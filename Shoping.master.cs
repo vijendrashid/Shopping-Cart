26a3592cc04b1ef53aeeb1a13d952f1e7b490291
unknown <Don@Don-PC.(none)>
2012-03-26 13:07:16 +0530

    first commit

---
+++ Shoping.master.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
+
+public partial class Shoping : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
